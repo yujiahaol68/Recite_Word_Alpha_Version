238b2f06383a6f1b846b2b89b4d822322f79555b
Sherlock.Yu <767954505@qq.com>
2015-02-06 23:58:45 -0800

    Initial Commit

---
+++ Recite_Word_Alpha_Version/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Recite_Word_Alpha_Version
+//
+//  Created by Sherlock.Yu on 15-2-6.
+//  Copyright (c) 2015年 Sherlock.Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
